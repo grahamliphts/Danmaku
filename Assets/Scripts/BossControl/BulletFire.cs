ced91044658ce9bf97d48ebe1362dc1f70c80ae3
pornin@hotmail.fr <banane32>
2015-10-05 20:46:49 +0200

    Bullet Pattern and management Added

--- Assets/Scripts/BossControl/BulletFire.cs
+++ Assets/Scripts/BossControl/BulletFire.cs
@@ -6,8 +6,9 @@ public class BulletFire : MonoBehaviour
     public GameObject[] bullets;
     private float m_timmer;
     private BulletPath[] m_Bullets_Script;
+    private int m_direction;
 
-    
+
     void Start()
     {
         // m_Bullet_Amount = 10;
@@ -16,9 +17,9 @@ public class BulletFire : MonoBehaviour
         {
             bullets[j].SetActive(false);
             m_Bullets_Script[j] = bullets[j].GetComponent<BulletPath>();
-            Debug.Log("Instantiating");
+            //Debug.Log("Instantiating");
         }
-        
+
         m_timmer = Time.time;
     }
 
@@ -27,13 +28,182 @@ public class BulletFire : MonoBehaviour
 
         if (Time.time - m_timmer > 0.5)
         {
-            for (int i = 0; i < bullets.Length; i++)
-            {
-                if(bullets[i].activeSelf == false)
-                    bullets[i].SetActive(true);
-                m_timmer = Time.time;
-            }
+            //StartCoroutine(circleSpin(0,bullets.Length));
+            //StartCoroutine(doubleSpin(bullets.Length,0));
+            //StartCoroutine(crossFire(bullets.Length,0));
+            //StartCoroutine(starFire(bullets.Length, 0));
+            //StartCoroutine(twinFire(bullets.Length, 0));
+            m_timmer = Time.time;
         }
     }
+
+    IEnumerator circleSpin(int max,int spawn = 0)
+    {
+        while (true)
+        {
+            if (spawn >= bullets.Length)
+                spawn = 0;
+            if (bullets[spawn].activeSelf == false)
+            {
+                bullets[spawn].SetActive(false);
+                bullets[spawn].transform.position = new Vector3(0, 0, 0);
+                bullets[spawn].SetActive(true);
+                m_Bullets_Script[spawn].play(m_direction);
+
+
+                spawn++;
+                if (m_direction < 360)
+                    m_direction += 5;
+                else
+                    m_direction = 0;
+            }
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    IEnumerator doubleSpin(int max,int spawn = 0)
+    {
+        while (true)
+        {
+            if (spawn >= max)
+                spawn = 0;
+            if (bullets[spawn].activeSelf == false)
+            {
+                if (spawn % 2 == 0)
+                {
+                    bullets[spawn].SetActive(false);
+                    bullets[spawn].transform.position = new Vector3(0, 0, 0);
+                    bullets[spawn].SetActive(true);
+                    m_Bullets_Script[spawn].play(m_direction);
+
+
+                    spawn++;
+                }
+                else
+                {
+
+                    bullets[spawn].SetActive(false);
+                    bullets[spawn].transform.position = new Vector3(0, 0, 0);
+                    bullets[spawn].SetActive(true);
+                    m_Bullets_Script[spawn].play(-m_direction);
+                    spawn++;
+                }
+                if (m_direction < 360)
+                    m_direction += 5;
+                else
+                    m_direction = 0;
+            }
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    IEnumerator crossFire(int max, int spawn = 0)
+    {
+        while (true)
+        {
+            if (spawn >= max)
+                spawn = 0;
+            if (bullets[spawn].activeSelf == false)
+            {
+                if (spawn % 2 == 0)
+                {
+                    bullets[spawn].SetActive(false);
+                    bullets[spawn].transform.position = new Vector3(0, 0, 0);
+                    bullets[spawn].SetActive(true);
+                    m_Bullets_Script[spawn].play(m_direction);
+
+
+                    spawn++;
+                }
+                else
+                {
+
+                    bullets[spawn].SetActive(false);
+                    bullets[spawn].transform.position = new Vector3(0, 0, 0);
+                    bullets[spawn].SetActive(true);
+                    m_Bullets_Script[spawn].play(-m_direction);
+                    spawn++;
+                }
+                if (m_direction < 360)
+                    m_direction += 90;
+                else
+                    m_direction = 0;
+            }
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    IEnumerator starFire(int max, int spawn = 0)
+    {
+        while (true)
+        {
+            if (spawn >= max)
+                spawn = 0;
+            if (bullets[spawn].activeSelf == false)
+            {
+                if (spawn % 2 == 0)
+                {
+                    bullets[spawn].SetActive(false);
+                    bullets[spawn].transform.position = new Vector3(0, 0, 0);
+                    bullets[spawn].SetActive(true);
+                    m_Bullets_Script[spawn].play(m_direction);
+
+
+                    spawn++;
+                }
+                else
+                {
+
+                    bullets[spawn].SetActive(false);
+                    bullets[spawn].transform.position = new Vector3(0, 0, 0);
+                    bullets[spawn].SetActive(true);
+                    m_Bullets_Script[spawn].play(-m_direction);
+                    spawn++;
+                }
+                if (m_direction < 360)
+                    m_direction += 45;
+                else
+                    m_direction = 0;
+            }
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    IEnumerator twinFire(int max, int spawn = 0)
+    {
+        while (true)
+        {
+            if (spawn >= max)
+                spawn = 0;
+            if (bullets[spawn].activeSelf == false)
+            {
+                if (spawn % 2 == 0)
+                {
+                    bullets[spawn].SetActive(false);
+                    bullets[spawn].transform.position = new Vector3(0, 0, 0);
+                    bullets[spawn].SetActive(true);
+                    m_Bullets_Script[spawn].play(m_direction);
+
+
+                    spawn++;
+                }
+                else
+                {
+
+                    bullets[spawn].SetActive(false);
+                    bullets[spawn].transform.position = new Vector3(0, 0, 0);
+                    bullets[spawn].SetActive(true);
+                    m_Bullets_Script[spawn].play(-m_direction);
+                    spawn++;
+                }
+                if (m_direction < 360)
+                    m_direction += 50;
+                else
+                    m_direction = 0;
+            }
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
 
 }
--- Assets/Scripts/BossControl/BulletPath.cs
+++ Assets/Scripts/BossControl/BulletPath.cs
@@ -19,19 +19,34 @@ public class BulletPath : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(Vector3.right * Time.deltaTime * speed);
+        gameObject.transform.Translate(Vector3.right * Time.deltaTime * speed);
         var pos = Camera.main.WorldToViewportPoint(transform.position);
-        Debug.Log(pos.x);
+        //Debug.Log(pos.x);
         if (pos.x >= 1 || pos.y >= 1 || pos.x <= 0 || pos.y <= 0)
             reset();
     }
 
-    void reset()
+    public void reset()
     {
         m_direction = Random.Range(0, 360);
         transform.position = new Vector3(0, 3, 0);
         transform.rotation = Quaternion.AngleAxis(m_direction, new Vector3(0, 0, 1));
         gameObject.SetActive(false);
+    }
+
+    public void stop()
+    {
+        gameObject.SetActive(false);
+        transform.position = new Vector3(0, 3, 0);
+    }
+
+    public void play(int direction)
+    {
+        m_direction = Random.Range(0, 360);
+        gameObject.SetActive(true);
+        transform.position = new Vector3(0, 3, 0);
+        transform.rotation = Quaternion.AngleAxis(direction, new Vector3(0, 0, 1));
+        
     }
 
 }
