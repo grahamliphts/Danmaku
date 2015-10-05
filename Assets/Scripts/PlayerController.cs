6869229ca9b27fbf2200a7cd21a055888af03b0b
ravonyx <ravonyxa@gmail.com>
2015-10-05 15:33:19 +0200

    gui quit + player controls with translate

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,9 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
     private float _movementSpeed = 4.0f;
-    private Vector2 _direction;
-    private Rigidbody2D _rigidbody;
 
     private float _dist;
     private float _leftBorder, _rightBorder, _upBorder, _bottomBorder;
-	// Use this for initialization
-	void Start ()
-    {
-        _rigidbody = GetComponent<Rigidbody2D>();  
-    }
 	
     void Update()
     {
@@ -21,26 +14,34 @@ public class PlayerController : MonoBehaviour
         viewPos.x = Mathf.Clamp01(viewPos.x);
         viewPos.y = Mathf.Clamp01(viewPos.y);
         transform.position = Camera.main.ViewportToWorldPoint(viewPos);
-        Debug.Log(transform.position);
-
-    }
-	// Update is called once per frame
-	void FixedUpdate ()
-    {
-        _direction = Vector3.zero;
         if (Input.GetKey("z"))
-            _direction += (Vector2)transform.up;
+            MoveUp();
         if (Input.GetKey("s"))
-            _direction -= (Vector2)transform.up;
+            MoveDown();
         if (Input.GetKey("q"))
-            _direction -= (Vector2)transform.right;
+            MoveLeft();
         if (Input.GetKey("d"))
-            _direction += (Vector2)transform.right;
-
-        _direction.Normalize();
-        _direction *= _movementSpeed;
-
-        _rigidbody.velocity = _direction;
-        Vector3 velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y);
+            MoveRight();
     }
+
+    void MoveUp()
+    {
+        transform.Translate((Vector2.up * _movementSpeed) * Time.deltaTime);
+    }
+
+    void MoveDown()
+    {
+        transform.Translate((-(Vector2.up) * _movementSpeed) * Time.deltaTime);
+    }
+
+    void MoveLeft()
+    {
+        transform.Translate((-(Vector2.right) * _movementSpeed) * Time.deltaTime);
+    }
+
+    void MoveRight()
+    {
+        transform.Translate((Vector2.right * _movementSpeed) * Time.deltaTime);
+    }
+
 }
