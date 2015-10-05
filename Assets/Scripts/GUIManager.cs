be35e102d8c226007383504be8770841fe04c096
ravonyx <ravonyxa@gmail.com>
2015-10-05 14:59:06 +0200

    menu scene

---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIManager : MonoBehaviour
+{
+
+    public void LoadGame()
+    {
+        Debug.Log("LoadGame");
+        Application.LoadLevel("MainScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
