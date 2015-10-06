65559464395db17762cf1f11195b4286ca074475
ravonyx <ravonyxa@gmail.com>
2015-10-06 22:55:42 +0200

    Life Boss begin

---
+++ Assets/Scripts/BossControl/BossLife.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossLife : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _life;
+    [SerializeField]
+    private int _damage;
+
+    void Start()
+    {
+        _life = 100;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //does not work now // create a taf for players shoot
+        if (col.gameObject.tag == "FriendProjectile")
+        {
+            if (_life > 0)
+                _life = _life - _damage;
+        }
+       
+    }
+
+    public float GetLife()
+    {
+        return _life;
+    }
+}
---
+++ Assets/Scripts/LifeManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LifeManager : MonoBehaviour {
+
+    public Scrollbar lifeBossValue;
+    public RawImage lifeBarBoss;
+
+    //color/value of life
+    private Color _lifeBarColor;
+    private float _life;
+    private float _lifeBarValue;
+
+    public BossLife lifeBoss;
+    void Update()
+    {
+        if (lifeBoss.GetLife() >= 0)
+        {
+            _lifeBarValue = lifeBoss.GetLife() / 100;
+            lifeBossValue.size = _lifeBarValue;
+            SetColorLife();
+            lifeBarBoss.color = _lifeBarColor;
+        }
+    }
+    void SetColorLife()
+    {
+        if (_lifeBarValue >= 0.5f)
+            _lifeBarColor = Color.green;
+        else if (_lifeBarValue >= 0.25f && _lifeBarValue < 0.5f)
+            _lifeBarColor = Color.yellow;
+        else if (_lifeBarValue < 0.25)
+            _lifeBarColor = Color.red;
+    }
+}
