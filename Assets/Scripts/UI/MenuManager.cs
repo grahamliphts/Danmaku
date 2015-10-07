ec8c252dbd9be7e126b226cd667d8a9de1f48899
ravonyx <ravonyxa@gmail.com>
2015-10-07 11:12:27 +0200

    Menu Options

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour 
+{
+    public Menu currentMenu;
+
+	void Start () 
+    {
+		Time.timeScale = 1;
+        ShowMenu(currentMenu);
+	}
+
+	public void ShowMenu (Menu menu) 
+    {
+        if (currentMenu != null)
+            currentMenu.IsOpen = false;
+        currentMenu = menu;
+        currentMenu.IsOpen = true;
+	}
+}
